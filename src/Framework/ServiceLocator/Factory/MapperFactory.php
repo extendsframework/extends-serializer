dc36e3f51fe47ff43ddfaf00b82a75db61686db9
Vincent van Dijk <info@vincentvandijk.com>
2019-10-28 20:04:32 +0100

    Don't overwrite config and added @throws.

--- src/Framework/ServiceLocator/Factory/MapperFactory.php
+++ src/Framework/ServiceLocator/Factory/MapperFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace ExtendsFramework\Serializer\Framework\ServiceLocator\Factory;
 
 use ExtendsFramework\Serializer\Mapper\Mapper;
+use ExtendsFramework\Serializer\Mapper\MapperException;
 use ExtendsFramework\Serializer\Mapper\MapperInterface;
 use ExtendsFramework\ServiceLocator\Resolver\Factory\ServiceFactoryInterface;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
@@ -12,14 +13,14 @@ class MapperFactory implements ServiceFactoryInterface
 {
     /**
      * @inheritDoc
+     * @throws MapperException
      */
     public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): object
     {
         $config = $serviceLocator->getConfig();
-        $config = $config[MapperInterface::class];
 
         $mapper = new Mapper();
-        foreach ($config as $className => $identifier) {
+        foreach ($config[MapperInterface::class] ?? [] as $className => $identifier) {
             $mapper->addMapping($className, $identifier);
         }
 
